45d439cc94edc6edfdb81cbb219eb194775a7ea9
Bovic101 <Victorbolanle72@gmail.com>
2025-02-20 11:25:35 +0100

    updating of the main function

--- cpp5/ex01/main.cpp
+++ cpp5/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 13:06:32 by victor-linu       #+#    #+#             */
-/*   Updated: 2025/02/20 11:13:39 by victor-linu      ###   ########.fr       */
+/*   Updated: 2025/02/20 11:18:57 by victor-linu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main()
 {
     BUREAUCRAT winner("Adeola", 75);//Adeola with grade 75
-    Form examform("Exam form", 60, 40 );//form is signe with grade 60 and exec at 20
+    Form examform("Exam form", 76, 40 );//form is signe with grade 60 and exec at 40
     std::cout << winner <<std::endl;
     std::cout << examform <<std::endl; //print exam form details
     winner.signForm(examform);//try to sign form for adeola
