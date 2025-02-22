c7d109f47f5315674e0d9964a840ed8aa8ae9837
Bovic101 <Victorbolanle72@gmail.com>
2025-02-22 13:24:25 +0100

    Implementaion of the data header

---
+++ cpp6/Data.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 13:16:07 by victor-linu       #+#    #+#             */
+/*   Updated: 2025/02/22 13:22:59 by victor-linu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATA_HPP
+# define DATA_HPP
+
+# include <iostream>
+# include <string>
+typedef struct s_data
+{
+	std::string str1;
+	std::string str2;
+}	Data;
+
+#endif
