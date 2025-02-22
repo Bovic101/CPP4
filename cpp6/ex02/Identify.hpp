b6e51cc8ce5729d3c2df11b9d7967cc29201d930
Bovic101 <Victorbolanle72@gmail.com>
2025-02-22 14:57:29 +0100

    Implemention changes and implemtation of the header that comtain the clases

--- cpp6/ex02/Identify.hpp
+++ cpp6/ex02/Identify.hpp
@@ -1,21 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Base.hpp                                           :+:      :+:    :+:   */
+/*   Identify.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/22 14:31:25 by victor-linu       #+#    #+#             */
-/*   Updated: 2025/02/22 14:33:35 by victor-linu      ###   ########.fr       */
+/*   Created: 2025/02/22 14:46:02 by victor-linu       #+#    #+#             */
+/*   Updated: 2025/02/22 14:47:42 by victor-linu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BASE_HPP
-#define BASE_HPP
+#ifndef IDENTIFY_HPP
+#define IDENTIFY_HPP
 
-class Base{
-    public:
-    virtual ~Base(); //Virtual destructir needed  for cleanup when del derived obj.
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+/*Base class with virtual destructor neede for polymorphism*/
+class Base {
+public:
+    virtual ~Base() {}
 };
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+// Function declarations
+Base* generate();
+void identify(Base* p);
+void identify(Base& p);
 
 #endif
