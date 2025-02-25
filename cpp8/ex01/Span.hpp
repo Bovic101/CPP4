7dcdcb522aa59ab60e45da4170f5e7c1fdc826e8
Bovic101 <Victorbolanle72@gmail.com>
2025-02-25 16:14:27 +0100

    Implementation of the header file by declaring the constrcutor and desstructor using OCF

---
+++ cpp8/ex01/Span.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 15:53:26 by victor-linu       #+#    #+#             */
+/*   Updated: 2025/02/25 16:01:20 by victor-linu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+class Span {
+private:
+    unsigned int _maxLen;
+    std::vector<int> _value;
+
+public:
+    Span(); //Default constructor
+    Span(unsigned int N);//paratḿerised constructor
+    Span(const Span& cp);//copy constructor
+    Span& operator=(const Span& cp);
+    ~Span();
+
+    // functions
+    void addNumber(int n);
+    int shortestSpan();
+    int longestSpan();
+
+    // Exception handling
+    class CustomException1 : public std::exception {
+    public:
+        virtual const char* what() const throw() {
+            return "Span is full";
+        }
+    };
+
+    class CustomException2 : public std::exception {
+    public:
+        virtual const char* what() const throw() {
+            return "No span can be found.";
+        }
+    };
+};
+
+#endif
