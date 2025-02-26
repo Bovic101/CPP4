19d29e8466c9358f739b39e343d369a020634c2e
Bovic101 <Victorbolanle72@gmail.com>
2025-02-26 15:30:49 +0100

    addition of a test case using list in the main.cpp file

--- cpp8/ex02/main.cpp
+++ cpp8/ex02/main.cpp
@@ -6,12 +6,13 @@
 /*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 10:17:28 by victor-linu       #+#    #+#             */
-/*   Updated: 2025/02/26 15:00:38 by victor-linu      ###   ########.fr       */
+/*   Updated: 2025/02/26 15:29:07 by victor-linu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
-
+#include <list>
+/*
 int	main(void)
 {
 	MutantStack<int> mstack;
@@ -37,3 +38,28 @@ int	main(void)
 	std::stack<int> s(mstack);
 	return (0);
 }
+    */
+   
+    /*Testing with the std:.list*/
+    int main(void)
+    {
+        std::list<int> mList;
+        mList.push_back(5);
+        mList.push_back(17);
+        std::cout << mList.back() <<std::endl; //it output the last element on stack
+        mList.pop_back();
+        std::cout << mList.size() << std::endl;
+        mList.push_back(3);
+        mList.push_back(5);
+        mList.push_back(737);
+        mList.push_back(0);
+        std::list<int>::iterator it = mList.begin();
+        std::list<int>::iterator ite = mList.end();
+        
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        return(0);
+    }
