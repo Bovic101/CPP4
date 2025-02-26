e351f791cd4c5e0124bdb11006d3d17fa960cb0a
Bovic101 <Victorbolanle72@gmail.com>
2025-02-26 22:18:45 +0100

    Bitcoin heade implementation

--- cpp9/ex00/BitcoinExchange.cpp
+++ cpp9/ex00/BitcoinExchange.cpp
@@ -6,7 +6,11 @@
 /*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 20:34:38 by victor-linu       #+#    #+#             */
-/*   Updated: 2025/02/26 20:34:39 by victor-linu      ###   ########.fr       */
+/*   Updated: 2025/02/26 22:15:26 by victor-linu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+Bitcoin::Bitcoin() = default;
+Bitcoin::Bitcoin(const Bitcoin &cp) = default;
--- cpp9/ex00/BitcoinExchange.hpp
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -6,7 +6,37 @@
 /*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 20:35:03 by victor-linu       #+#    #+#             */
-/*   Updated: 2025/02/26 20:35:04 by victor-linu      ###   ########.fr       */
+/*   Updated: 2025/02/26 22:13:19 by victor-linu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <regex>
+#include <fstream>
+#include <optional>//handle missing date
+#include <filesystem> //check if dile exit
+#include <string>
+
+class Bitcoin{
+    private:
+    std::map<std::string, double> Xchangerate;
+    
+    bool DateValidator(const std::string& date);
+    bool ValueValidator(const std::string &val);
+    std::optional<std::string>SearchForLowerDate(const std::string &datum);
+    
+    public:
+    Bitcoin();
+    Bitcoin(const Bitcoin& cp);
+    Bitcoin &operator=(const Bitcoin &cp);
+    
+    void ExchangeRateStack(const std::string& f);
+    void InputFileProcessor(const std::string& f);
+};
+
+#endif
