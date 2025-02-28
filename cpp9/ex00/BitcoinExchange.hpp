228032f1fae5992f69db0f5f4ccbf55e70129e30
Bovic101 <Victorbolanle72@gmail.com>
2025-02-28 11:20:00 +0100

    addition of additional method to the heaeder file and the complete implementation of the main function

--- cpp9/ex00/BitcoinExchange.hpp
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -6,37 +6,48 @@
 /*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 20:35:03 by victor-linu       #+#    #+#             */
-/*   Updated: 2025/02/26 22:13:19 by victor-linu      ###   ########.fr       */
+/*   Updated: 2025/02/28 11:17:38 by victor-linu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
-#define BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
 
-#include <iostream>
-#include <sstream>
-#include <map>
-#include <regex>
-#include <fstream>
-#include <optional>//handle missing date
-#include <filesystem> //check if dile exit
-#include <string>
+# include <filesystem>
+# include <fstream>
+# include <iostream>
+# include <map>
+# include <optional>
+# include <regex>
+# include <sstream>
+# include <string>
 
-class Bitcoin{
-    private:
-    std::map<std::string, double> Xchangerate;
-    
-    bool DateValidator(const std::string& date);
-    bool ValueValidator(const std::string &val);
-    std::optional<std::string>SearchForLowerDate(const std::string &datum);
-    
-    public:
-    Bitcoin();
-    Bitcoin(const Bitcoin& cp);
-    Bitcoin &operator=(const Bitcoin &cp);
-    
-    void ExchangeRateStack(const std::string& f);
-    void InputFileProcessor(const std::string& f);
+class Bitcoin
+{
+  private:
+	std::map<std::string, double> Xchangerate; // Search closest lower date with exchange rate
+
+  public:
+	Bitcoin();
+	Bitcoin(const Bitcoin &cp);
+	Bitcoin &operator=(const Bitcoin &cp);
+	~Bitcoin();
+
+	// Load exchange rates from file
+    void ExchangeRateStack(const std::string &f); 
+
+    //Process input values from file	
+	void InputFileProcessor(const std::string &f);
+
+	bool DateValidator(const std::string &date); // Check for the date format
+	bool ValueValidator(const std::string &val); // check the value
+
+	// Search closest lower date with exchange rate
+    std::optional<std::string> SearchForLowerDate(const std::string &datum);
+		
 };
 
 #endif
+
+
+
--- cpp9/ex00/main.cpp
+++ cpp9/ex00/main.cpp
@@ -6,7 +6,22 @@
 /*   By: victor-linux <victor-linux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 20:34:08 by victor-linu       #+#    #+#             */
-/*   Updated: 2025/02/26 20:34:09 by victor-linu      ###   ########.fr       */
+/*   Updated: 2025/02/28 11:18:25 by victor-linu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Error: Usage: ./btc <csv_file\n";
+        return(1);
+    }
+
+    Bitcoin coin;
+    coin.ExchangeRateStack("data.csv");//To extract rate from the csv file
+    coin.InputFileProcessor(argv[1]);
+    return(0);
+}
